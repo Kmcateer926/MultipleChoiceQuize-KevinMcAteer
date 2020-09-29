c9a4eaaa13faf03ff1431fb2f5e445b0bae21c12
Kevin McAteer <kmcateer926@gmail.com>
2020-09-29 19:17:41 -0400

    added render functions but it is broken, figuring out why now

--- script.js
+++ script.js
@@ -3,48 +3,54 @@ var welcomeContainer = document.getElementById("welcome-container");
 var answerContainer = document.getElementById("answer-container");
 var questionContainer = document.getElementById("question-container");
 var correctAnswer = document.getElementById("correct-answer");
+var button = document.getElementById("start-quiz");
+var timerDisplay = document.getElementById("timer")
 
-var questions = 0;
+var questionIndex = 0;
 
-var quizQuestions = 0; [
+var quizQuestions = [
     {
         question: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts",
     },
+    {
+        question: "The condition in an if / else statement is enclosed within _______.",
+        choices: ["quotes", "curly brackets", "parentheses", "squareBrackets"],
+        answer: "curly brackets", 
+    }
 ]
 
-function renderQuestions() {
-    questionContainer.innerHTML ="";
+function renderQuizQuestions() {
+    questionContainer.innerHTML = "";
     var question = document.createElement("h2");
-    question.textContent = questions[quizQuestions].question;
-    console.log(Questions[quizQuestions].question);
+    question.textContent = quizQuestions[questionIndex].question;
+    console.log(quizQuestions[questionIndex].question);
     questionContainer.append(question)
     renderChoices();
 }
 
+function renderChoices() {
+    console.log(choices);
+    for (var i = 0; i <= quizQuestions[questionIndex].choices.length; i++) {
+        var choiceButtons =document.createElement("button");
+        choiceButtons.setAttribute("style", "background-color: purple; color: white; margin: 5px");
+        choiceButtons.setAttribute("data-value", quizQuestions[questionIndex].choices[i]);
+        choiceButtons.textContent = quizQuestions[questionIndex].choices[i];
+        answerContainer.append(choiceButtons);
 
-function highScore(score) {
-  var saved = 0;
-  try {
-    saved = parseFloat(localStorage.highScore);
-  } catch (e) {
-    saved = 0;
-  }
-  if (!(typeof score === "undefined")) {
-    try {
-      score = parseFloat(score);
-    } catch (e) {
-      score = 0;
+
     }
-    if (score > saved) {
-      saved = score;
-      localStorage.highScore = "" + score;
-    }
-  }
-  if (isNaN(saved)) {
-    saved = 0;
-    localStorage.highScore = "0";
-  }
-  return saved;
 }
+
+
+button.addEventListener("click", function () {
+    welcomeContainer.style.display = "none";
+    questionContainer.setAttribute("style", "display:block");
+    questionContainer.textContent = quizQuestions.questions;
+    answerContainer.setAttribute("style", "display:block");
+    answerContainer.textContent = quizQuestions.choices;
+    renderQuizQuestions();
+});
+
+
