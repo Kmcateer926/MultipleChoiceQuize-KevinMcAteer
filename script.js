ec6779ec4635279588df56ad89e72653b3aa3186
Kevin McAteer <kmcateer926@gmail.com>
2020-09-29 17:51:11 -0400

    added variables in javascript

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("hello world")
+// console.log("hello world"); connected
+var welcomeContainer = document.getElementById("welcome-container");
+var answerContainer = document.getElementById("answer-container");
+var questionContainer = document.getElementById("question-container");
+var correctAnswer = document.getElementById("correct-answer");
+
+
+
+
+
+
+
+
+
+
+
+
+
+function highScore(score) {
+    var saved = 0;
+    try { saved = parseFloat(localStorage.highScore); } catch (e) { saved = 0; }
+    if (!(typeof score === 'undefined')) {
+       try { score = parseFloat(score); } catch (e) { score = 0; }
+       if (score>saved) {
+         saved = score;
+         localStorage.highScore = '' + score;
+       }
+    }
+    if (isNaN(saved)) {
+       saved = 0;
+       localStorage.highScore = '0';
+    }
+    return saved;
+ }
